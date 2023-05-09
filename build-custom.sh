7f6cb900aac16ee9ae37cefc67fc14745ed248a9
Joshua Scheck <49527681+jschec@users.noreply.github.com>
2023-05-09 12:17:13 -0700

    Updated build-custom.sh script

--- build-custom.sh
+++ build-custom.sh
@@ -6,6 +6,10 @@ fi
 COMPONENT_NAME=$1
 VERSION=$2
 
+# set artifact tags
+FRONTEND_TAG=inventory-manager-webserver
+BACKEND_TAG=inventory-manager-backend
+
 # copy recipe to greengrass-build
 cp recipe.yaml ./greengrass-build/recipes
 
@@ -19,8 +23,8 @@ docker-compose -f docker-compose.yaml build
 cd ..
 
 # save Docker images as tar
-docker save --output ./custom-build/$COMPONENT_NAME/flask-app.tar flask-app
-docker save --output ./custom-build/$COMPONENT_NAME/react-webapp.tar react-webapp
+docker save --output ./custom-build/$COMPONENT_NAME/$FRONTEND_TAG.tar $FRONTEND_TAG
+docker save --output ./custom-build/$COMPONENT_NAME/$BACKEND_TAG.tar $BACKEND_TAG
 
 # include docker-compose.yaml in archive
 cp src/docker-compose.yaml ./custom-build/$COMPONENT_NAME/
